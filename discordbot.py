08843594f5b6fae5dc9ee4130d6a336a32658f9e
zen0831 <zenzenzen087@gmail.com>
2022-07-06 23:47:48 +0900

    aa

--- discordbot.py
+++ discordbot.py
@@ -18,20 +18,22 @@ async def on_command_error(ctx, error):
     error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
     result = tr.translate(error_msg, src="en", dest="ja").text
     await ctx.send(result)
-
+in 
 
 
 @bot.command()
 async def ping(ctx):
     await ctx.send('テストぉ')
 
+@bot.command()
 async def en(ctx):
     text = tr.translate(message.content, src="en", dest="ja").text
     await ctx.send(text)
 
+@bot.command()
 async def text(ctx):
-    await ctx.channel.send('**音楽botについて**')
-    await ctx.channel.send('🟥基本コマンドの説明\n\n"m!join" -音楽botをVCに入れます\n"m!play" -リンクの動画再生\n"m!skip" -再生中の動画をスキップします\n"m!leave" -VCからbotを蹴ります\n"m!setup" -botの設定\n"m!loop" -音楽のループ再生\n\n\n🟥コマンドの使い方\n\n例)\nm!play https://youtu.be/xxxxxx')
+    await ctx.send('**音楽botについて**')
+    await ctx.send('🟥基本コマンドの説明\n\n"m!join" -音楽botをVCに入れます\n"m!play" -リンクの動画再生\n"m!skip" -再生中の動画をスキップします\n"m!leave" -VCからbotを蹴ります\n"m!setup" -botの設定\n"m!loop" -音楽のループ再生\n\n\n🟥コマンドの使い方\n\n例)\nm!play https://youtu.be/xxxxxx')
     
 
 
